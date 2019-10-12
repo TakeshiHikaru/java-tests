2e3fbeaffcd21b1bc1d7a34ff6218d61640d7ebf
MatheusFrez <matheusfrez.nf@gmail.com>
2019-10-12 10:51:41 -0300

    Adição classes de teste CLiente DAO no teste de regressão

--- tests-java/test/br/com/cefet/testes/suite/RegressaoTestSuite.java
+++ tests-java/test/br/com/cefet/testes/suite/RegressaoTestSuite.java
@@ -4,10 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import br.com.cefet.banco.persistencia.bd.ClienteDAOTest;
+
 @RunWith(Suite.class)
 @SuiteClasses({ 
 	BancoNegocioTestSuite.class, 
-	BancoTestSuite.class 
+	BancoTestSuite.class,
+	ClienteDAOTest.class
 })
 public class RegressaoTestSuite {
 
