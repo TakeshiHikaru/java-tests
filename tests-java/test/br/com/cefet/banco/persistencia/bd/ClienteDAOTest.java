6e9906f2f997fc6836049adcad6acccbd26912b6
MatheusFrez <matheusfrez.nf@gmail.com>
2019-10-12 10:50:56 -0300

    Início implementação de testes nas classes de DAO

---
+++ tests-java/test/br/com/cefet/banco/persistencia/bd/ClienteDAOTest.java
@@ -0,0 +1,41 @@
+package br.com.cefet.banco.persistencia.bd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import br.com.cefet.banco.negocio.Caixa;
+import br.com.cefet.banco.negocio.Cliente;
+
+public class ClienteDAOTest {
+
+	// Utilizando a técnica dos caminhos básicos
+	@Test
+	public void testBuscaClientePeloId() {
+		ClienteDAO dao = new ClienteDAO();
+		Cliente cliente = dao.getCliente(1);
+		assertEquals("clienteTest", cliente.getNome());
+	}
+	
+	// Utilizando a técnica dos caminhos básicos
+	@Test
+	public void testBuscaClientePeloUsuario() {
+		ClienteDAO dao = new ClienteDAO();
+		Cliente cliente = dao.getCliente("cliente");
+		assertEquals("clienteTest", cliente.getNome());
+	}
+	
+	// Utilizando a técnica dos caminhos básicos 
+	// NÃO FUNCIONANDO
+	@Test
+	public void testExclusaoUsuario() {
+		ClienteDAO dao = new ClienteDAO();
+		Cliente clienteTest = new Cliente("cliente exclusão", "endereço", "111.111.111-11", "clienteexclusao", "clienteexclusao");
+		dao.adicionaCliente(clienteTest);
+		Cliente cliente = dao.getCliente("cliente exclusão");
+		dao.remove(cliente);
+		assertEquals(null, dao.getCliente("cliente exclusão"));
+	}
+
+
+}
