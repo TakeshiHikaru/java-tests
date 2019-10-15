b3dcc062f3a1741048ffa5738ffc27493658623f
MatheusFrez <matheusfrez.nf@gmail.com>
2019-10-14 21:27:29 -0300

    Criação do pacote de apresentação

---
+++ tests-java/test/br/com/cefet/banco/apresentacao/PainelCadastrarFuncionarioTest.java
@@ -0,0 +1,14 @@
+package br.com.cefet.banco.apresentacao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PainelCadastrarFuncionarioTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
