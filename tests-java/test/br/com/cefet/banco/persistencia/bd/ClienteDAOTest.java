df938bef385d92dcd8a491927e539eed4cf3cfae
MatheusFrez <matheusfrez.nf@gmail.com>
2019-10-15 19:52:05 -0300

    Primeiro teste de classe DAO finalizado

--- tests-java/test/br/com/cefet/banco/persistencia/bd/ClienteDAOTest.java
+++ tests-java/test/br/com/cefet/banco/persistencia/bd/ClienteDAOTest.java
@@ -2,39 +2,67 @@ package br.com.cefet.banco.persistencia.bd;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.cefet.banco.negocio.Caixa;
 import br.com.cefet.banco.negocio.Cliente;
+import br.com.cefet.banco.negocio.ContaCorrente;
+import br.com.cefet.banco.negocio.ContaPoupanca;
 
 public class ClienteDAOTest {
+	
+	ClienteDAO clienteDAO;
+	
+	//A tabela de contas deve estar vazia
+	@Before
+	public void inicializaConta() {
+		this.clienteDAO = new ClienteDAO();
+	}
 
 	// Utilizando a técnica dos caminhos básicos
 	@Test
 	public void testBuscaClientePeloId() {
-		ClienteDAO dao = new ClienteDAO();
-		Cliente cliente = dao.getCliente(1);
-		assertEquals("Nome", cliente.getNome());
+		Cliente cliente = this.clienteDAO.getCliente(1);
+		assertEquals("clienteTest", cliente.getNome());
 	}
 	
 	// Utilizando a técnica dos caminhos básicos
 	@Test
-	public void testBuscaClientePeloUsuario() {
-		ClienteDAO dao = new ClienteDAO();
-		Cliente cliente = dao.getCliente("userTest");
+	public void testBuscaClientePeloNome() {
+		Cliente cliente = this.clienteDAO.getCliente("userTest");
 		assertEquals("Nome", cliente.getNome());
 	}
 	
 	// Utilizando a técnica dos caminhos básicos 
-	// NÃO FUNCIONANDO
 	@Test
-	public void testExclusaoUsuario() {
-		ClienteDAO dao = new ClienteDAO();
+	public void testExclusaoCliente() {
 		Cliente clienteTest = new Cliente("cliente exclusao", "endereço", "111.111.111-11", "clienteexclusao", "clienteexclusao");
-		dao.adicionaCliente(clienteTest);
-		Cliente cliente = dao.getCliente("clienteexclusao");
-		dao.remove(cliente);
-		assertNull(dao.getCliente("cliente exclusão"));
+		this.clienteDAO.adicionaCliente(clienteTest);
+		Cliente cliente = this.clienteDAO.getCliente("clienteexclusao");
+		this.clienteDAO.remove(cliente);
+		assertNull(this.clienteDAO.getCliente("cliente exclusão"));
+	}
+	
+	// Utilizando a técnica dos caminhos básicos 
+	// Lista não pode ser vazia por quê o banco já vai ter alguns dados de cliente
+	@Test
+	public void testObentacaoListaClientes() {
+		ClienteDAO dao = new ClienteDAO();
+		assertTrue(!dao.getListaClientes().isEmpty());
+	}
+	
+	// Utilizando a técnica dos caminhos básicos 
+	@Test
+	public void testAlteracaoDeCliente() {
+		Cliente cliente = this.clienteDAO.getCliente(2);
+		cliente.setNome("teste alteração");
+		this.clienteDAO.altera(cliente);
+		Cliente clienteAlterado = this.clienteDAO.getCliente(2);
+		assertEquals("teste alteração", clienteAlterado.getNome());
 	}
 
 
