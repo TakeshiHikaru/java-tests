c0a944d78ded6d940e95826e7dd8f34a9016647b
MatheusFrez <matheusfrez.nf@gmail.com>
2019-10-19 22:24:53 -0300

    Resolução problema git

--- tests-java/test/br/com/cefet/testes/suite/BancoTestSuite.java
+++ tests-java/test/br/com/cefet/testes/suite/BancoTestSuite.java
@@ -5,13 +5,15 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import br.com.cefet.banco.negocio.BancoTest;
 import br.com.cefet.banco.util.BancoUtilTest;
 import junit.framework.TestSuite;
 
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	BancoUtilTest.class
+	BancoUtilTest.class,
+	BancoTest.class
 })
 
 public class BancoTestSuite {
