67b409b72cc66cf7823eb2efebfb64a2092ea97c
MatheusFrez <matheusfrez.nf@gmail.com>
2019-10-26 11:39:44 -0300

    Melhoria testes de tela

--- tests-java/test/br/com/cefet/banco/apresentacao/PainelClienteTest.java
+++ tests-java/test/br/com/cefet/banco/apresentacao/PainelClienteTest.java
@@ -6,21 +6,36 @@ import org.junit.Before;
 import org.junit.Test;
 
 import br.com.cefet.banco.negocio.Cliente;
+import br.com.cefet.banco.persistencia.bd.ClienteDAO;
 
 public class PainelClienteTest {
 
-	PainelCliente tela;
 
-	// técnica de percorrer os caminhos básicos
+	ClienteDAO clienteDAO;
+	
 	@Before
-	public void inicializaTest() {
-		Cliente cliente = new Cliente("cliente", "111.111.111-11", "endereço", "cliente", "clientesenha");
-		this.tela = new PainelCliente(cliente);
+	public void inicializaConta() {
+		this.clienteDAO = new ClienteDAO();
 	}
 	
+	// técnica de percorrer os caminhos básicos
+	// TEM QUE TER UMA CONTA NO BANCO PARA ESSE CLIENTE 1 COM O NOME clienteTest
 	@Test
-	public void testeQualquer() {
-		
+	public void testaInicializacaoTelaCliente() {
+		Cliente cliente = this.clienteDAO.getCliente(1);
+		PainelCliente tela = new PainelCliente(cliente);
+		assertEquals("clienteTest", tela.getClienteLogado().getNome());
 	}
+	
+	
+	// técnica de percorrer os caminhos básicos
+	// TEM QUE TER UMA CONTA NO BANCO PARA ESSE CLIENTE COM VALOR 20
+	@Test
+	public void testaObtencaoContaCliente() {
+		Cliente cliente = this.clienteDAO.getCliente(1);
+		PainelCliente tela = new PainelCliente(cliente);
+		assertEquals(20.0, tela.getConta().getSaldo(), 0.0001);
+	}
+	
 
 }
--- tests-java/test/br/com/cefet/banco/apresentacao/TelaPrincipalTest.java
+++ tests-java/test/br/com/cefet/banco/apresentacao/TelaPrincipalTest.java
@@ -7,21 +7,42 @@ import org.junit.Test;
 
 import br.com.cefet.banco.negocio.Caixa;
 import br.com.cefet.banco.negocio.Cliente;
+import br.com.cefet.banco.negocio.Diretor;
+import br.com.cefet.banco.negocio.Gerente;
 
 public class TelaPrincipalTest {
 
-	TelaPrincipal tela;
-
-	// técnica de percorrer os caminhos básicos
-	@Before
-	public void inicializaTest() {
-		Caixa caixa = new Caixa("caixa", "", "", "", "" , "", 1500);
-		this.tela = new TelaPrincipal(caixa);
+	
+	// teste utilizando técnica de todas as decisões
+	@Test
+	public void testaCriacaoTelaPrincipalUsuarioCliente() {
+		Cliente cliente = new Cliente("cliente", "", "", "", "");
+		TelaPrincipal tela = new TelaPrincipal(cliente);
+		assertEquals("cliente", tela.getNomeUsuario());
 	}
 	
+	// teste utilizando técnica dos caminhos básicos e todas as decisões
 	@Test
-	public void testeQualquer() {
-		
+	public void testaCriacaoTelaPrincipalUsuarioCaixa() {
+		Caixa caixa = new Caixa("caixa", "", "", "", "" , "caixa", 1500);
+		TelaPrincipal tela = new TelaPrincipal(caixa);
+		assertEquals("caixa", tela.getNomeUsuario());
+	}
+	
+	// teste utilizando técnica dos caminhos básicos e todas as decisões
+	@Test
+	public void testaCriacaoTelaPrincipalUsuarioGerente() {
+		Gerente gerente = new Gerente("gerente", "", "", "", "" , "gerente", 5000);
+		TelaPrincipal tela = new TelaPrincipal(gerente);
+		assertEquals("gerente", tela.getNomeUsuario());
+	}
+	
+	// teste utilizando técnica dos caminhos básicos e todas as decisões
+	@Test
+	public void testaCriacaoTelaPrincipalUsuarioDiretor() {
+		Diretor diretor = new Diretor("diretor", "", "", "", "" , "diretor", 10000);
+		TelaPrincipal tela = new TelaPrincipal(diretor);
+		assertEquals("diretor", tela.getNomeUsuario());
 	}
 
 }
